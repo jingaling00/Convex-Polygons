e6f0f2a42e520229bee7d0b2499646ff434b0626
Jingyan Zhang <105741006+jingaling00@users.noreply.github.com>
2022-11-07 15:38:09 -0500

    added docstrings

--- convexpolygon.py
+++ convexpolygon.py
@@ -24,22 +24,41 @@ def orient2d(a, b, c):
         and 0 if collinear
         
     '''
-    # Signed area of triangle formed by a,b,c
-    s_a = (b.x - a.x) * (c.y - a.y) - (c.x - a.x) * (b.y - a.y)
-    
-    # Orientation
-    if s_a > 0:
-        result = 1
-    elif s_a < 0:
-        result = -1
-    else:
-        result = 0
-    
-    return result
+    if isinstance(a, P) and isinstance(b, P) and isinstance(c, P): # check if inputs are Point objects
+        # Signed area of triangle formed by a,b,c
+        s_a = (b.x - a.x) * (c.y - a.y) - (c.x - a.x) * (b.y - a.y)
+        
+        # Orientation
+        if s_a > 0:
+            result = 1
+        elif s_a < 0:
+            result = -1
+        else:
+            result = 0
+        
+        return result
 
 class ConvexPolygon:
-    def __init__(self, points=[]):
+    """ Creates an n-sided convex polygon with a list of points
+        Polygon can be scaled, rotated, translated, etc. 
+        using vector and point objects from Vec2D and Point classes"""
         
+    def __init__(self, points=[]):
+        """
+        Parameters
+        ----------
+        Takes in a list of points as parameter. 
+        The default is [].
+        Uses the list of points to generate edges,
+        which are vectors between the points.
+
+        Raises
+        ------
+        ValueError is raised when the list of points does not
+        form a convex polygon (i.e., the number of points is less than 3,
+        the points do not lie in the same orientation, etc.)
+
+        """
         self.verts = points
         
         self.edges = []
@@ -47,9 +66,9 @@ class ConvexPolygon:
         if len(self.verts) < 3:
             raise ValueError('Points do not form a convex polygon')
         
-        self.nverts = len(self.verts)
+        self.nverts = len(self.verts) # total number of points
             
-        for i in range(0, self.nverts):
+        for i in range(0, self.nverts): # generating the list of edges using Vec2D class
             if i == self.nverts - 1:
                 vector = V(self.verts[i], self.verts[0])
                 self.edges.append(vector)
@@ -57,7 +76,7 @@ class ConvexPolygon:
                 vector = V(self.verts[i], self.verts[i+1])
                 self.edges.append(vector)
         
-        for i in range(self.nverts):
+        for i in range(self.nverts): # checking if points form a convex polygon
             orient_sum = 0
             for j in range(self.nverts):
                 orient = orient2d(self.verts[i], self.verts[(i+1) % self.nverts], self.verts[j])
@@ -67,6 +86,13 @@ class ConvexPolygon:
                 raise ValueError('Points do not form a convex polygon')
                      
     def __str__(self):
+        """
+        Returns
+        -------
+        str object is returned by formatting the polygon's
+        vertices and edges.
+        """
+        
         verts = []
         for i in self.verts:
             verts.append(P.__str__(i))
@@ -76,16 +102,35 @@ class ConvexPolygon:
         for i in self.edges:
             edges.append(V.__str__(i))
         edges_str = ', '.join(edges)
+        
         return f'No. of Vertices: {self.nverts}\nVertices {vert_str}\nEdges {edges_str}'
 
     def translate(self, vec2d):
+        """
+        Parameters
+        ----------
+        Takes a Vec2D (vector) object as parameter.
+
+        Returns
+        -------
+        Using vector parameter, the shape is translated along this vector.
+        The list of vertices, and edges, is redefined.
+
+        """
+        
         new_verts = []
         for n in self.verts:
             n += vec2d
             new_verts.append(n)
         self.verts = new_verts
     
-    def centroid(self): # make one loop
+    def centroid(self):
+        """
+        Returns
+        -------
+        Returns the Point object denoting the centroid of the polygon.
+
+        """
         a = 0
         cx = 0
         cy = 0
@@ -112,6 +157,17 @@ class ConvexPolygon:
         return P(cx, cy)
     
     def rotate(self, angle, pivot=0):
+        """
+        Rotates shape according to angle and pivot point.
+        
+        Parameters
+        ----------
+        angle : angle in radians indicating the extent of rotation.
+        pivot : the Point object around which the shape will be rotated. 
+                The default is the shape's own centroid.
+
+        """
+        
         if pivot == 0:
             pivot = self.centroid()
         verts = []
@@ -134,6 +190,16 @@ class ConvexPolygon:
                 self.edges.append(vector)   
         
     def scale(self, sx, sy):
+        """
+        The shape is stretched/shrunken according to a stretch constant in parameters.
+
+        Parameters
+        ----------
+        sx : the factor by which the x-coordinates are stretched/shrunken.
+        sy : the factor by which the y-coordinates are streched/shrunken.
+
+        """
+        
         centroid = self.centroid()
         verts = []
         for v in self.verts:
@@ -153,6 +219,21 @@ class ConvexPolygon:
                 self.edges.append(vector)
     
     def __and__(self, other):
+        """
+        Overloads the & operator by using vector projections to analyze
+        whether two convex polygons will overlap in a coordinate plane.
+
+        Parameters
+        ----------
+        self, other are two convex polygon objects.
+
+        Returns
+        -------
+        bool: True denotes the polygons do overlap. False denotes polygons
+        do not overlap.
+
+        """
+        
         total_edges = self.edges + other.edges
         
         for edge in total_edges:
@@ -180,4 +261,3 @@ class ConvexPolygon:
                 return False
         
         return True
-    
